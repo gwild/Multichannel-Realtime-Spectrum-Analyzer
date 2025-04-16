75dac08c6e9cf9186c9b63784719dd542c5c3eb0
gwild <gwildes1@gmail.com>
2025-04-16 14:19:37 -0500

    path

--- build.rs
+++ build.rs
@@ -1,28 +1,23 @@
 fn main() {
-    // Disable JACK support at compile time
-    println!("cargo:rustc-cfg=pa_disable_jack");
+    // Disable JACK support at compile time since we don't need it on Windows
     println!("cargo:rustc-env=PA_DISABLE_JACK=1");
     println!("cargo:rustc-env=PA_USE_JACK=0");
-    println!("cargo:rustc-env=PA_USE_ALSA=1");
+    println!("cargo:rustc-env=PA_USE_WASAPI=1"); // Use WASAPI on Windows
     println!("cargo:rustc-env=PA_BUILD_SHARED=0");
 
-    // System library paths
-    println!("cargo:rustc-link-search=native=/usr/lib");
+    // Add the path where JACK is installed
     println!("cargo:rustc-link-search=native=/usr/lib/aarch64-linux-gnu");
-    println!("cargo:rustc-link-search=native=/usr/local/lib");  // For locally built libraries
-    println!("cargo:rustc-link-search=native=/usr/lib/arm-linux-gnueabihf");  // Alternative ARM path
     
-    // Link against ALSA
-    println!("cargo:rustc-link-lib=asound");
-    
-    // Link against JACK with full path specification
-    println!("cargo:rustc-link-search=native=/usr/lib/jack");  // JACK specific directory
+    // Link against JACK
     println!("cargo:rustc-link-lib=jack");
-    
+
     // Link against PortAudio
     println!("cargo:rustc-link-lib=static=portaudio");
 
-    // Set compile-time flags for PortAudio
-    println!("cargo:rustc-cfg=feature=\"pa_disable_jack\"");
-    println!("cargo:rustc-cfg=feature=\"pa_use_alsa\"");
+    // Windows-specific settings
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=ole32");
+        println!("cargo:rustc-link-lib=setupapi");
+        println!("cargo:rustc-link-lib=winmm");
+    }
 } 
