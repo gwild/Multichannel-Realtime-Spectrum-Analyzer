cb9f6d082cd3919211f2ba0b9ccc96cd8c1f678b
gwild <gwildes1@gmail.com>
2025-04-16 14:34:47 -0500

    fix

--- build.rs
+++ build.rs
@@ -1,23 +1,14 @@
 fn main() {
-    // Disable JACK support at compile time since we don't need it on Windows
-    println!("cargo:rustc-env=PA_DISABLE_JACK=1");
-    println!("cargo:rustc-env=PA_USE_JACK=0");
-    println!("cargo:rustc-env=PA_USE_WASAPI=1"); // Use WASAPI on Windows
-    println!("cargo:rustc-env=PA_BUILD_SHARED=0");
-
-    // Add the path where JACK is installed
+    // Add system library paths
+    println!("cargo:rustc-link-search=native=/usr/lib");
     println!("cargo:rustc-link-search=native=/usr/lib/aarch64-linux-gnu");
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
     
-    // Link against JACK
+    // Link against system libraries for Raspberry Pi
     println!("cargo:rustc-link-lib=jack");
-
-    // Link against PortAudio
+    println!("cargo:rustc-link-lib=asound");
     println!("cargo:rustc-link-lib=static=portaudio");
-
-    // Windows-specific settings
-    if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-lib=ole32");
-        println!("cargo:rustc-link-lib=setupapi");
-        println!("cargo:rustc-link-lib=winmm");
-    }
+    
+    // Set rpath to ensure libraries can be found at runtime
+    println!("cargo:rustc-link-arg=-Wl,-rpath=/usr/lib/aarch64-linux-gnu");
 } 
