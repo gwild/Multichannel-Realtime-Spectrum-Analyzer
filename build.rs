0ef71085d3f7b21d6211358727bca41e12ebaa5c
gwild <gwildes1@gmail.com>
2025-04-16 13:48:11 -0500

    jackdisable

--- build.rs
+++ build.rs
@@ -1,10 +1,16 @@
 fn main() {
-    // Disable JACK support in PortAudio
+    // Configure PortAudio build without JACK support
     println!("cargo:rustc-env=PA_DISABLE_JACK=1");
+    println!("cargo:rustc-cfg=pa_disable_jack");
     
-    // Link against ALSA
+    // Link against ALSA (Linux sound system)
     println!("cargo:rustc-link-lib=asound");
     
+    // Configure PortAudio build options
+    println!("cargo:rustc-env=PA_USE_JACK=0");
+    println!("cargo:rustc-env=PA_USE_ALSA=1");
+    println!("cargo:rustc-env=PA_BUILD_SHARED=0");
+    
     // Link against PortAudio
-    println!("cargo:rustc-link-lib=portaudio");
+    println!("cargo:rustc-link-lib=static=portaudio");
 } 
