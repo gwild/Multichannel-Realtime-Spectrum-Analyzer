be0af30c96a77abf9c85a4c6be41252033e8c040
gwild <gwildes1@gmail.com>
2025-04-16 13:35:30 -0500

    nojack

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    // Disable JACK support in PortAudio
+    println!("cargo:rustc-env=PA_DISABLE_JACK=1");
+    
     // Link against ALSA
     println!("cargo:rustc-link-lib=asound");
     
