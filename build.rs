beb786f305bd1e576ecc288c922a1d92251e50c7
gwild <gwildes1@gmail.com>
2025-04-16 15:29:31 -0500

    fix

--- build.rs
+++ build.rs
@@ -3,12 +3,14 @@ fn main() {
     println!("cargo:rustc-link-search=native=/usr/lib");
     println!("cargo:rustc-link-search=native=/usr/lib/aarch64-linux-gnu");
     println!("cargo:rustc-link-search=native=/usr/local/lib");
-    
-    // Link against system libraries for Raspberry Pi
-    println!("cargo:rustc-link-lib=jack");
-    println!("cargo:rustc-link-lib=asound");
+
+    // Link portaudio first (it references JACK)
     println!("cargo:rustc-link-lib=static=portaudio");
-    
+    // Then JACK
+    println!("cargo:rustc-link-lib=jack");
+    // Then ALSA
+    println!("cargo:rustc-link-lib=asound");
+
     // Set rpath to ensure libraries can be found at runtime
     println!("cargo:rustc-link-arg=-Wl,-rpath=/usr/lib/aarch64-linux-gnu");
-} 
+}
